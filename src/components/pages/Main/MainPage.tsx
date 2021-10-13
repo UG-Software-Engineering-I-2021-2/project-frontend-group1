5583e33096c65ecf87a06141b952b614d418ffe9
antoniott15 <antonio.toche@gmail.com>
2021-10-13 15:18:03 -0500

    fix(router): declare module react router

--- src/components/pages/Main/MainPage.tsx
+++ src/components/pages/Main/MainPage.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel, Box } from "@chakra-ui/react"
 
 export const MainPage = () => {
-    
+
     return (
         <>
         <Box >
             <img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Utec-logo.jpg" style={{marginBottom:"20px"}} width="150" height="200"></img>
         </Box>
+        
         <Tabs size="lg" variant="enclosed" isFitted>
             <TabList >
                 <Tab>Cursos</Tab>
